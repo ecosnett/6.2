3c53db37cc1b7b9babcd07f31a982e464938ce49
ecosnett <ecosnett@outlook.com>
2025-02-19 09:40:17 +0000

    commit

--- TestPingApp/obj/Debug/net8.0/TestPingApp.AssemblyInfo.cs
+++ TestPingApp/obj/Debug/net8.0/TestPingApp.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestPingApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a718336e87a8996f876b6f742c92a9b79c1e8025")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef0956466414f5f1349332623e1eaca08639cea0")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestPingApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestPingApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
