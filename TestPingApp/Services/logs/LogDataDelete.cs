dc3a6b7867d6cee6384cadb9aedb13f2d64d0e5f
ecosnett <ecosnett@outlook.com>
2025-02-19 10:05:25 +0000

    commit

--- TestPingApp/Services/logs/LogDataDelete.cs
+++ TestPingApp/Services/logs/LogDataDelete.cs
@@ -1,28 +1,11 @@
-<<<<<<< HEAD
-﻿using System.Data.SQLite;
-=======
-﻿using System;
+using System;
 using System.Data.SqlClient;
 using System.Threading.Tasks;
->>>>>>> azure/DEVELOPMENT
 
 namespace TestPingApp.Services.logs
 {
     public class LogDataDelete
     {
-<<<<<<< HEAD
-        public async static Task DeleteLog(DateTime timestamp, string url)
-        {
-            string _connectionString = "Data Source=D:\\Users\\edward\\codes\\C#\\PingWebApp\\sites.db;Version=3;BusyTimeout=30000;";
-
-            try
-            {
-                using (var conn = new SQLiteConnection(_connectionString))
-                {
-                    await conn.OpenAsync();
-
-                    using (var command = new SQLiteCommand("DELETE FROM site_logs WHERE url = @url AND timestamp = @timestamp", conn))
-=======
         private static readonly string _connectionString = $"Server={Config.Server};Database={Config.Database};User Id={Config.Username};Password={Config.Password};Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
 
         public async static Task DeleteLog(DateTime timestamp, string url)
@@ -34,19 +17,12 @@ namespace TestPingApp.Services.logs
                     await conn.OpenAsync();
 
                     using (var command = new SqlCommand("DELETE FROM logs WHERE url = @url AND timestamp = @timestamp", conn))
->>>>>>> azure/DEVELOPMENT
                     {
                         command.Parameters.AddWithValue("@url", url);
                         command.Parameters.AddWithValue("@timestamp", timestamp);
 
-<<<<<<< HEAD
-                        command.ExecuteNonQuery();
-                    }
-                    conn.Close();
-=======
                         await command.ExecuteNonQueryAsync();
                     }
->>>>>>> azure/DEVELOPMENT
                 }
             }
             catch (Exception ex)
--- TestPingApp/obj/Debug/net8.0/TestPingApp.AssemblyInfo.cs
+++ TestPingApp/obj/Debug/net8.0/TestPingApp.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestPingApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef0956466414f5f1349332623e1eaca08639cea0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c53db37cc1b7b9babcd07f31a982e464938ce49")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestPingApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestPingApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
